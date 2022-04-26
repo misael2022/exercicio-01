3d49b82139ca8f025a9d2e2208fd688e620fd5d7
misael2022 <ggmisajvrmichel@gmail.com>
2022-04-26 15:59:51 -0300

    exercicio-01

---
+++ 01-exerc.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Exercicios PHP</h1>
+    <hr>
+
+    <?php
+        //Variaveis
+        $data = "26/04/2022";
+        $nome = "Misael";
+        $idade = 15;
+        $cidade = "São Paulo";
+    ?>
+
+    <p>
+        Hoje e <?=$data?>. Meu nome e <?=$nome?>, tenho <?=$idade?> e mora na cidade de <?=$cidade?>.
+    </p>
+</body>
+</html>
